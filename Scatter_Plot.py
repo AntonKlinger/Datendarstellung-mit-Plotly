7b13bf33431a47bf232ce2381d2aecc91edcb8ed
anton <anton.klinger@web.de>
2023-03-20 13:19:31 +0100

    Hinzufügen von Fehlern auf der x- Achse

--- Scatter_Plot.py
+++ Scatter_Plot.py
@@ -2,10 +2,12 @@ import plotly.express as px
 
 
 df = px.data.iris()     #liefer einen im Programm enthaltenen Datensatz
+df["Fehler"] = df["sepal_width"]/50
 #print(df)
 
 fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",    # erstellen eines Scaller Plots
                  marginal_x="box", marginal_y="violin",                     # hinzufügen der grafischen Auswertung
-                 trendline="ols")                                           # hinzufügen der Trendlinie
+                 trendline="ols",
+                 error_x="Fehler")                                           # hinzufügen der Trendlinie
 
 fig.write_html('first_plot.html', auto_open=True)
