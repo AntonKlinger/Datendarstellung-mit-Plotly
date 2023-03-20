3352efcd769abb2db3fe13a8f2b93b1d402f473c
anton <anton.klinger@web.de>
2023-03-20 12:42:52 +0100

    Beschriftung des Diagramms

--- Bar_Diagramm.py
+++ Bar_Diagramm.py
@@ -4,6 +4,9 @@ graph = go.Figure(
     data=go.Bar(
         x=[0, 1, 2],
         y=[39, 94, 48]
+    ),
+    layout=go.Layout(
+        title=go.layout.Title(text="Diagramm")
     )
 )
 graph.write_html('first_plot.html', auto_open=True)
