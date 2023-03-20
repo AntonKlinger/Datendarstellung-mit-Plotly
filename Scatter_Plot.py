af8ca587362727bf203645c5fa0d08640527f9ce
anton <anton.klinger@web.de>
2023-03-20 13:12:42 +0100

    erstellen eines Scatter PlotS

--- Scatter_Plot.py
+++ Scatter_Plot.py
@@ -4,6 +4,8 @@ import plotly.express as px
 df = px.data.iris()     #liefer einen im Programm enthaltenen Datensatz
 #print(df)
 
-fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species")
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",    # erstellen eines Scaller Plots
+                 marginal_x="box", marginal_y="violin",                     # hinzufügen der grafischen Auswertung
+                 trendline="ols")                                           # hinzufügen der Trendlinie
 
 fig.write_html('first_plot.html', auto_open=True)
